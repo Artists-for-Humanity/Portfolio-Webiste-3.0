d9e3df746caca061f78f742cb34f6c7538ac6b73
jermy77 <simpguy34@gmail.com>
2024-05-28 17:47:21 -0400

    e

--- public/jeremy/jeremy.js
+++ public/jeremy/jeremy.js
@@ -2,11 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const crumbledPaper = document.querySelector(".crumbledPaper");
   const pageOptions = document.querySelector(".pageOptions");
 
+  
   crumbledPaper.addEventListener("mouseenter", function () {
-    pageOptions.style.display = "flex";
+    gt.style.display = "flex";
   });
 
   crumbledPaper.addEventListener("mouseleave", function () {
-    pageOptions.style.display = "none";
+    gt.style.display = "none";
   });
 });
